75159afda27d8f1cfb617f9391e5664f41c101e8
Aizkun <maxime.t13m@gmail.com>
2022-03-31 18:11:34 +0200

    dev:ADD models and controllers

---
+++ horse_sim/app/Models/Concours.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Concours extends Model
+{
+    use HasFactory;
+}
---
+++ horse_sim/app/Models/TacheAuto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TacheAuto extends Model
+{
+    use HasFactory;
+}
