15b99c133b4417041ef913c8a6cbb980ea711bf0
nguyenj-c <nguyenjeanchristophe951@gmail.com>
2022-04-07 12:56:01 +0200

    test

--- horse_sim/app/Models/Article.php
+++ horse_sim/app/Models/Article.php
@@ -4,24 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Venturecraft\Revisionable\Revisionable;
-use \Venturecraft\Revisionable\RevisionableTrait;
 
-class Article extends Revisionable {
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait, \Venturecraft\Revisionable\RevisionableTrait;
-
-    public function identifiableName()
-    {
-        return $this->name;
-    }
-
-    // If you are using another bootable trait
-    // be sure to override the boot method in your model
-    public static function boot()
-    {
-        parent::boot();
-    }
-
+class Article extends Model
+{
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
 
