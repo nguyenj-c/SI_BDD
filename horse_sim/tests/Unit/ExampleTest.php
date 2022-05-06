fc149fe34e66ac56c399f55c052aeeaa09581ef3
Jean Christophe Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2022-05-06 16:38:17 +0200

    yes review work

--- horse_sim/tests/Unit/ExampleTest.php
+++ horse_sim/tests/Unit/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 
     public function testEnvDatabaseConfigurationIsMysql()
